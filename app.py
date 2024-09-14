2494aad0fff99422cb5c205d9e39b095bfeff33e
pedrodevelops <dev.henriquegn@gmail.com>
2024-09-14 13:26:34 -0300

    feat: adiciona modos de jogo

--- app.py
+++ app.py
@@ -2,65 +2,91 @@ import time
 import os
 import random
 
-win_lose_escolhas = [('tesoura', 'papel'),
-                     ('papel', 'pedra'),
-                     ('pedra', 'tesoura'),
-                     ('pedra', 'lagarto'),
-                     ('lagarto', 'spock'),
-                     ('spock', 'tesoura'),
-                     ('tesoura', 'lagarto'),
-                     ('lagarto', 'papel'),
-                     ('papel', 'spock'),
-                     ('spock', 'pedra'),]
+win_lose_escolhas = [
+    ("tesoura", "papel"),
+    ("papel", "pedra"),
+    ("pedra", "tesoura"),
+    ("pedra", "lagarto"),
+    ("lagarto", "spock"),
+    ("spock", "tesoura"),
+    ("tesoura", "lagarto"),
+    ("lagarto", "papel"),
+    ("papel", "spock"),
+    ("spock", "pedra"),
+]
+
+escolhas_validas = ("pedra", "papel", "tesoura", "lagarto", "spock")
+
+
+def ler_escolha():
+    print("Digite um número abaixo: ")
+    print(" 0 -> pedra\n 1 -> papel\n 2 -> tesoura\n 3 -> lagarto\n 4 -> spock")
+
+    escolha = int(input("Sua escolha: "))
+
+    return escolhas_validas[escolha]
+
 
 def main():
-    modo = input('Escolha o modo de jogo: \n 1 -> Player x Machine\n 2 -> Player x Player\n')
-    tempo_duracao = input('Escolha a duração do jogo em segundos: \n 1 -> 30\n 2 -> 45\n 3 -> 60\n')
+    modo = input(
+        "Escolha o modo de jogo: \n 1 -> Player x Machine\n 2 -> Player x Player\n"
+    )
+    tempo_duracao = input(
+        "Escolha a duração do jogo em segundos: \n 1 -> 30\n 2 -> 45\n 3 -> 60\n"
+    )
 
-    tempo_duracao = int(tempo_duracao)
+    tempo_duracao = [30, 45, 60][int(tempo_duracao) - 1]
 
     # Estado do jogo
     pontos = [0, 0]
     tempo_inicial = time.time()
 
-    escolhas = ('pedra', 'papel', 'tesoura', 'lagarto', 'spock')
-
     while True:
-        os.system('clear')
+        os.system("clear")
         tempo_atual = time.time()
         if (tempo_atual - tempo_inicial) > tempo_duracao:
             break
 
-        escolha_computador = random.choice(escolhas)
+        escolhas = ["", ""]
 
-        print(f"Tempo restante: {tempo_duracao - int(tempo_atual - tempo_inicial)} segundos")
+        print(
+            f"Tempo restante: {tempo_duracao - int(tempo_atual - tempo_inicial)} segundos"
+        )
 
-        print('Digite um número abaixo: ')
-        print(' 0 -> pedra\n 1 -> papel\n 2 -> tesoura\n 3 -> lagarto\n 4 -> spock')
+        print("Vez do jogador 1")
+        escolhas[0] = ler_escolha()
+        if modo == "2":
+            print("Vez do jogador 2")
+            escolhas[1] = ler_escolha()
+        else:
+            escolhas[1] = random.choice(escolhas_validas)
 
-        jogador = int(input('Sua escolha: '))
+        print("Sua escolha foi: ", escolhas[0])
+        print("O computador escolheu: ", escolhas[1])
 
-        escolha_jogador = escolhas[jogador]
-
-
-        print('Sua escolha foi: ', escolha_jogador )
-        print('O computador escolheu: ', escolha_computador)
-
-        if escolha_computador == escolha_jogador:
+        if escolhas[0] == escolhas[1]:
             print("It's a tie!")
-        elif (escolha_jogador, escolha_computador) in win_lose_escolhas:
-            print('You won!')
+        elif (escolhas[0], escolhas[1]) in win_lose_escolhas:
+            print("Jogador 1 - You won!")
             pontos[0] += 1
         else:
+            if modo == "2":
+                print("Jogador 2 - You won!")
+            else:
+                print("Computador ganhou")
             pontos[1] += 1
-        time.sleep(1)
+        time.sleep(3)
 
-    resultado = 'You won!' if pontos[0] > pontos[1] else 'You lose!' if pontos[0] < pontos[1] else "It's a tie!"
+    resultado = (
+        "You won!"
+        if pontos[0] > pontos[1]
+        else "You lose!" if pontos[0] < pontos[1] else "It's a tie!"
+    )
 
     print(f"Placar final: {pontos[0]} x {pontos[1]}")
 
     print(resultado)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
