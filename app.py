ebe9b9cb5dd7f0a04304139c364d9add35c4e851
pedrodevelops <dev.henriquegn@gmail.com>
2024-09-15 12:15:44 -0300

    refactor: modularizar código

--- app.py
+++ app.py
@@ -18,14 +18,26 @@ win_lose_escolhas = [
 escolhas_validas = ("pedra", "papel", "tesoura", "lagarto", "spock")
 
 
-def ler_escolha(message):
-    print(message)
+def ler_escolha(mensagem):
+    print(mensagem)
     print(" 0 -> pedra\n 1 -> papel\n 2 -> tesoura\n 3 -> lagarto\n 4 -> spock")
 
     escolha = int(input("Sua escolha: "))
 
     return escolhas_validas[escolha]
 
+
+
+def mostrar_tempo_restante(tempo_duracao, tempo_inicial):
+    tempo_atual = time.time()
+    tempo_restante = tempo_duracao - int(tempo_atual - tempo_inicial)
+    print(f"Tempo restante: {tempo_restante} segundos")
+
+def mostrar_pontos(pontos):
+    print(f"Placar: {pontos[0]} x {pontos[1]}")
+
+def tempo_expirado(tempo_duracao, tempo_inicial):
+    return (time.time() - tempo_inicial) > tempo_duracao
 
 def main():
     modo = input(
@@ -44,19 +56,29 @@ def main():
     while True:
         os.system("clear")
         tempo_atual = time.time()
-        if (tempo_atual - tempo_inicial) > tempo_duracao:
+        if tempo_expirado(tempo_duracao, tempo_inicial):
             break
 
+        mostrar_tempo_restante(tempo_duracao, tempo_inicial)
+        mostrar_pontos(pontos)
+
         escolhas = ["", ""]
-
-        print(
-            f"Tempo restante: {tempo_duracao - int(tempo_atual - tempo_inicial)} segundos"
-        )
-
         escolhas[0] = ler_escolha("Jogador 1, sua jogada:")
+
+        if tempo_expirado(tempo_duracao, tempo_inicial):
+            print("Tempo esgotado!")
+            pontos[0] -= 1
+
+        print('\n\n')
+
 
         if modo == "2":
             escolhas[1] = ler_escolha("Jogador 2, sua jogada:")
+
+        if tempo_expirado(tempo_duracao, tempo_inicial):
+            print("Tempo esgotado!")
+            pontos[0] -= 1
+
         else:
             escolhas[1] = random.choice(escolhas_validas)
 
@@ -76,15 +98,16 @@ def main():
             pontos[1] += 1
         time.sleep(3)
 
-    resultado = (
-        "You won!"
-        if pontos[0] > pontos[1]
-        else "You lose!" if pontos[0] < pontos[1] else "It's a tie!"
-    )
+    print("Fim de jogo!")
+    mostrar_pontos(pontos)
 
-    print(f"Placar final: {pontos[0]} x {pontos[1]}")
+    vencedor = 1 if pontos[0] > pontos[1] else 2
 
-    print(resultado)
+    if modo == '1':
+        print('You win' if vencedor == 1 else 'You lose')
+    else:
+        print(f"Jogador {vencedor} venceu!")
+
 
 
 if __name__ == "__main__":
